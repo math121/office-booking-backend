ccf109e83ca0e371b6e42d3f3f9e7914e8f83f40
Mathangi Pushparajah <mathangi23@outlook.com>
2024-07-15 08:50:06 +0200

    Initial commit

---
+++ src/main/java/hackweek/office_booking_backend/OfficeBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package hackweek.office_booking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeBookingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hackweek/office_booking_backend/OfficeBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hackweek.office_booking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
