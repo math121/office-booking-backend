734f2174d5d357bf2527b99c578c00e4cfd1a2a0
Mathangi Pushparajah <mathangi23@outlook.com>
2024-07-16 09:46:14 +0200

    remove deployment - back to localhost

--- src/test/java/hackweek/office_booking_backend/OfficeBookingBackendApplicationTests.java
+++ src/test/java/hackweek/office_booking_backend/OfficeBookingBackendApplicationTests.java
@@ -1,13 +1,6 @@
 package hackweek.office_booking_backend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class OfficeBookingBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
